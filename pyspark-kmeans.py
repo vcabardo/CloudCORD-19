ea72f0bd4bcecb5d2d56ef7a9beab311dd04581a
Vensan Cabardo <42554910+vcabardo@users.noreply.github.com>
2022-04-29 12:40:41 -0600

    adding more preprocessing

--- pyspark-kmeans.py
+++ pyspark-kmeans.py
@@ -1,5 +1,3 @@
-#%%configure -f
-#{"driverMemory": "600M"}
 # Resources
 # Pyspark k-means example: https://github.com/apache/spark/blob/master/examples/src/main/python/ml/kmeans_example.py
 # EMR setup help(mentions how to get a notebook): https://towardsdatascience.com/data-science-at-scale-with-pyspark-on-amazon-emr-cluster-622a0f4534ed
@@ -8,24 +6,34 @@
 # Vectorization: https://spark.apache.org/docs/latest/ml-features
 
 import argparse
-# import matplotlib.pyplot as plt
 from pyspark.sql import SparkSession
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.evaluation import ClusteringEvaluator
 from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF
+from pyspark.sql.functions import lower, regexp_replace, col
 
+# sc.install_pypi_package("six==1.16.0")
+# sc.install_pypi_package("matplotlib", "https://pypi.org/simple")
+# import matplotlib.pyplot as plt
 
 with SparkSession.builder.appName("Cluster research papers").getOrCreate() as spark:
-
     dataset = spark.read.format("csv") \
       .option("sep", ",") \
       .option("inferSchema", "true") \
       .option("header", "true") \
       .load("s3://ccassignment2/cleaned.csv")
 
-    #tokenize
+    column_name = "abstract"
+
+    #preprocessing: make all letters lowercase and remove special characters from the documents
+    lowercase_df = dataset.select(lower(col("abstract"), lower(col("title")).alias("abstract"))
+    no_punct_df = lowercase_df.select((regexp_replace(col("abstract"), "[^a-z0-9\\s]", "")).alias("abstract"))
+
+    #preprocessing: tokenize
     tokenizer = Tokenizer(inputCol="abstract", outputCol="words")
-    wordsData = tokenizer.transform(dataset)
+    wordsData = tokenizer.transform(no_punct_df)
+
+    #preprocessing: remove stop words
     remover = StopWordsRemover(inputCol="words", outputCol="filteredWords")
     removed = remover.transform(wordsData)
 
@@ -39,11 +47,11 @@ with SparkSession.builder.appName("Cluster research papers").getOrCreate() as sp
     result = idfModel.transform(featurizedData)
 
     #print to check if it works
-    result.select("features").show()
+    result.select("filteredWords", "features").show()
 
     # Figure out best K
 #     silhouetteScores = []
-#     for K in range(2,11):
+#     for K in range(7,8):
 #         #compute model with K
 #         kmeans = KMeans().setK(K).setSeed(1)
 #         model = kmeans.fit(result)
@@ -55,19 +63,23 @@ with SparkSession.builder.appName("Cluster research papers").getOrCreate() as sp
 #         silhouetteScores.append(silhouette)
 
 #     fig, ax = plt.subplots(1,1, figsize =(10,8))
-#     ax.plot(range(2,11),silhouetteScores)
+#     ax.plot(range(7,8),silhouetteScores)
 #     ax.set_xlabel('Number of Clusters')
 #     ax.set_ylabel('Silhouette Score')
+#     plt.show()
 
     #Create final kmeans model - assignment says to create 8 clusters
     kmeans = KMeans().setK(8).setSeed(1)
     model = kmeans.fit(result)
     predictions = model.transform(result)
+    predictions.show()
 
     #Print result
-    centers = model.clusterCenters()
-    print("Cluster Centers: ")
-    for center in centers:
-        print(center)
+#     centers = model.clusterCenters()
+#     print("Cluster Centers: ")
+#     for center in centers:
+#         print(center)
+#         print(type(center))
+
 
     spark.stop()
